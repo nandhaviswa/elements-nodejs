2952b1a5848f71d468dc6f2562c01c0bc2c3015f
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2021-08-07 20:31:50 +0530

    applogger integrated

--- index.js
+++ index.js
@@ -13,19 +13,32 @@ morgan.token('moment', function (req) {
 
 const routes = require('./routes');
 
+// create a rolling file logger based on date/time that fires process events
+const opts = {
+    // errorEventName:'error',
+    logDirectory: path.join(__dirname, 'public', 'logs'), // NOTE: folder must exist and be writable...
+    fileNamePattern:'application-<DATE>.log',
+    dateFormat:'YYYY-MM-DD',
+    timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS'
+};
+const applogger = require('simple-node-logger').createRollingFileLogger( opts );
+app.set('applogger',applogger);
+
+applogger.error('hi')
+
 // create a rotating write stream
 var accessLogStream = rfs.createStream('access.log', {
-  interval: '1d', // rotate daily
-  path: path.join(__dirname, 'public', 'logs')
+    interval: '1d', // rotate daily
+    path: path.join(__dirname, 'public', 'logs')
 })
 
 // setup the logger
 app.use(morgan(':moment :remote-addr :remote-user :method :url :status :res[content-length] - :response-time ms', {
-	 stream: accessLogStream,
+    stream: accessLogStream,
 }))
 
 app.use('/',routes);
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Elements app listening at http://localhost:${port}`)
+    console.log(`Elements app listening at http://localhost:${port}`)
 })
