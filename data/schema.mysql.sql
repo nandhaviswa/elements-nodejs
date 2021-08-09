c0bbd5a6f2334159118cb99433d5aee623a4dfc5
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2021-08-09 09:25:38 +0530

    schema updated

--- data/schema.mysql.sql
+++ data/schema.mysql.sql
@@ -1,9 +1,11 @@
+DROP DATABASE IF EXISTS `elements`;
 CREATE DATABASE `elements`;
 
 DROP USER IF EXISTS 'elements'@'127.0.0.1';
 CREATE USER 'elements'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY 'elements';
 GRANT ALL PRIVILEGES ON *.* TO 'elements'@'127.0.0.1' WITH GRANT OPTION;
 
+USE elements;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
