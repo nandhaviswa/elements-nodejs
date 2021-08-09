ea8ffcf1a021375e13f3d43c74d8addee3fb6f54
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2021-08-09 10:00:16 +0530

    all methods implemented

--- routes/user.js
+++ routes/user.js
@@ -3,23 +3,102 @@ var router = express.Router()
 const User = require('./../models/user');
 
 router.post('/', function (req, res) {
-  res.send('user/post');
+    let body = req.body;
+    var user = new User();
+    user.username = body.username;
+    user.email = body.email;
+    user.save().then(() => {
+        res.json({
+            status: true,
+            msg: 'User has been created successfully!',
+            data: user,
+        });
+    }).catch( err => {
+        res.json({
+            status: false,
+            msg: 'Error while creating user.',
+            data: err,
+        });
+    }).finally(() => res.end());
 })
 
-router.get('/', function (req, res) {
-  res.send('user/get');
+router.get('/:id', function (req, res) {
+    let id = req.param('id',null);
+    User.findByPk(id).then( user => {
+        res.json({
+            status: true,
+            msg: `get user #${id}`,
+            data: user,
+        });
+    }).catch( err => {
+        res.json({
+            status: false,
+            msg: 'Error while getting user.',
+            data: err,
+        });
+    }).finally(() => res.end());
 })
 
-router.put('/', function (req, res) {
-  res.send('user/update');
+router.put('/:id', function (req, res) {
+    let id = req.param('id',null);
+    User.findByPk(id).then( user => {
+        if(user == null){
+            throw new Error(`User is found for the id #${id}`);
+        }
+        let body = req.body;
+        user.username = body.username;
+        user.email = body.email;
+        return user.save();
+    }).then( user => {
+        res.json({
+            status: true,
+            msg: `get user #${id}`,
+            data: user,
+        });
+    }).catch( err => {
+        res.json({
+            status: false,
+            msg: 'Error while getting user.',
+            data: err,
+        });
+    }).finally(() => res.end());
 })
 
-router.delete('/', function (req, res) {
-  res.send('user/delete');
+router.delete('/:id', function (req, res) {
+    let id = req.param('id',null);
+    User.destroy({
+        where: {
+            id: id,
+        }
+    }).then( count => {
+        res.json({
+            status: true,
+            msg: `get user #${id}`,
+            data: count,
+        });
+    }).catch( err => {
+        res.json({
+            status: false,
+            msg: 'Error while getting user.',
+            data: err,
+        });
+    }).finally(() => res.end());
 })
 
 router.get('/index', function (req, res) {
-  res.send('user/index');
+    User.findAll().then( users => {
+        res.json({
+            status: true,
+            msg: 'List of users',
+            data: users,
+        });
+    }).catch( err => {
+        res.json({
+            status: false,
+            msg: 'Error while listings users.',
+            data: err,
+        });
+    }).finally(() => res.end());
 })
 
 module.exports = router
